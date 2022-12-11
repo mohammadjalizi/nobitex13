bc147682413b9c46ff62b6c03a0f7f5b6d403b3f
mohammadjalizi <mjalizi1376@gmail.com>
2022-12-11 18:00:38 +0330

    addproject

--- java.js
+++ java.js
@@ -16,3 +16,39 @@ console.log(document.documentElement.scrollTop)
 })
 
 AOS.init();
+
+
+
+// swiper
+const swiper = new Swiper('.test', {
+  slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+  navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        
+        },
+        breakpoints:{
+
+640:{
+  slidesPerView: 1,
+  spaceBetween: 20,
+},
+768:{
+  slidesPerView: 2,
+  spaceBetween: 30,
+
+},
+1024:{
+
+  slidesPerView: 3,
+  spaceBetween: 30,
+}
+
+        }
+});
